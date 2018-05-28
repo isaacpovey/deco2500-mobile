5f5ca08762b4ebc10f5e7cba605eeba44477b912
Isaac Povey <isaac.povey@gmail.com>
2018-05-28 17:45:56 +1000

    Updated Dashboard with buttons

--- app/src/main/java/com/u1/group2/unirideshare/Dashboard.java
+++ app/src/main/java/com/u1/group2/unirideshare/Dashboard.java
@@ -7,9 +7,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.u1.group2.unirideshare.components.TripViewAdapter;
 import com.u1.group2.unirideshare.datamodels.DriverTrip;
@@ -29,6 +30,7 @@ public class Dashboard extends AppCompatActivity {
     private RecyclerView tripsRecyclerView;
     private RecyclerView.Adapter tripsAdapter;
     private RecyclerView.LayoutManager tripsLayoutManager;
+    private TextView noTrips;
 
 
     @Override
@@ -36,6 +38,7 @@ public class Dashboard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        noTrips = findViewById(R.id.notTrips);
         this.tripsRecyclerView = findViewById(R.id.tripsView);
         this.tripsRecyclerView.setHasFixedSize(true);
         this.tripsLayoutManager = new LinearLayoutManager(this);
@@ -60,12 +63,20 @@ public class Dashboard extends AppCompatActivity {
             }
         }
 
+        if (driverTrips.isEmpty() && riderTrips.isEmpty()) {
+            this.noTrips.setVisibility(View.VISIBLE);
+        } else {
+            this.noTrips.setVisibility(View.INVISIBLE);
+        }
+
         ArrayList<Trip> tripsData = new ArrayList<Trip>();
         tripsData.addAll(this.driverTrips.values());
         tripsData.addAll(this.riderTrips.values());
 
         this.tripsAdapter = new TripViewAdapter(tripsData);
         this.tripsRecyclerView.setAdapter(this.tripsAdapter);
+
+
 
 
         FloatingActionButton fab = findViewById(R.id.fab);
